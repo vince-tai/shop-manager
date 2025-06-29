36ae560c84e8444f51c26d780d1511af40c9a24e
Vincent Tai <177089640+vince-tai@users.noreply.github.com>
2025-06-29 12:49:39 +0800

    Initial commit

---
+++ src/test/java/com/github/vincetai/shop_manager/ShopManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.vincetai.shop_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
