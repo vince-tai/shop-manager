5b6f32b5a8d965686cc61173058fca00af94d8d6
Vincent Tai <177089640+vince-tai@users.noreply.github.com>
2025-08-12 23:39:22 +0800

    add banner.txt

--- src/main/java/com/github/vince_tai/shop_manager/Product.java
+++ src/main/java/com/github/vince_tai/shop_manager/Product.java
@@ -1,5 +1,28 @@
 package com.github.vince_tai.shop_manager;
 
 public class Product {
-    
+    String name;
+    double price;
+
+    public Product(String name, double price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getPrice() {
+            return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
 }
